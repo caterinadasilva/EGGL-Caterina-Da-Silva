344608cb1b46b3340df5331370fcad6782ed7019
Caterina <caterinadasilva@outlook.com>
2012-09-01 08:57:05 -0400

    Entrega de Proyecto
    Entrega final de propuesta de portada para el sitio de la Escuela de
    Arquitectura y Diseño e[ad].

---
+++ js/c_config.js
@@ -0,0 +1,45 @@
+// USE WORDWRAP AND MAXIMIZE THE WINDOW TO SEE THIS FILE
+c_styles={};c_menus={}; // do not remove this line
+
+// You can remove most comments from this file to reduce the size if you like.
+
+
+
+
+/******************************************************
+	(1) GLOBAL SETTINGS
+*******************************************************/
+
+c_hideTimeout=500; // 1000==1 second
+c_subShowTimeout=250;
+c_keepHighlighted=true;
+c_findCURRENT=false; // find the item linking to the current page and apply it the CURRENT style class
+c_findCURRENTTree=true;
+c_overlapControlsInIE=true;
+c_rightToLeft=false; // if the menu text should have "rtl" direction (e.g. Hebrew, Arabic)
+
+
+
+/******************************************************
+	(3) MENU TREE FEATURES
+*******************************************************/
+
+// Normally you would probably have just one menu tree (i.e. one main menu with sub menus).
+// But you are actually not limited to just one and you can have as many menu trees as you like.
+// Just copy/paste a config block below and configure it for another UL element if you like.
+
+
+c_menus['Menu1']=[ // the UL element with id="Menu1"
+[
+// MAIN-MENU FEATURES
+'horizontal',	// ItemsArrangement ('vertical','horizontal')
+'absolute',	// Position ('relative','absolute','fixed')
+'0',		// X Position (values in CSS valid units- px,em,ex)
+'0',		// Y Position (values in CSS valid units- px,em,ex)
+false,		// RightToLeft display of the sub menus
+false,		// BottomToTop display of the sub menus
+0,		// X SubMenuOffset (pixels)
+0,		// Y SubMenuOffset
+'MM',		// CSS Class (one of the defined in section 2)
+true		// Open sub-menus onclick (default is onmouseover)
+]
